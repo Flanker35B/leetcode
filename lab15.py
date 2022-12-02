c7c23adf295be570f35c1f2becc4956f42788afe
Flanker35B <116949314+Flanker35B@users.noreply.github.com>
2022-12-02 16:51:28 +0300

    Update lab15.py

--- lab15.py
+++ lab15.py
@@ -5,6 +5,27 @@ https://leetcode.com/problems/sort-the-people/
 '''
 
 
+def sorting(heights):
+    if len(heights) <= 1:
+            return heights
+    left = []
+    right = []
+    center = []
+    elem = heights[0][1]
+    for item in heights:
+        if item[1] < elem:
+            left.append(item)
+        elif item[1] > elem:
+            right.append(item)
+        else:
+            center.append(item)
+
+    return sorting(right) + center + sorting(left)
+    
+    
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-      return list(a for b,a in sorted (zip(heights,names),reverse=True))
+       
+        new_array =[(n, h) for n, h in zip(names, heights)]
+        res = sorting(new_array)
+        return [ n for n, h in res]
