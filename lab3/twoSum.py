57dd9b559e017a83a49e1cf498a4b9a64ddbaf25
Flanker35B <116949314+Flanker35B@users.noreply.github.com>
2022-12-14 23:38:33 +0300

    Create twoSum.py

---
+++ lab3/twoSum.py
@@ -0,0 +1,14 @@
+'''
+Name: twoSum
+https://leetcode.com/problems/two-sum/
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pmap={}
+
+        for i, n in enumerate(nums):
+            d= target - n
+            if d in pmap:
+                return [pmap[d], i]
+            pmap[n]=i
